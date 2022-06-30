b16cc61246161cd72a384cc2d91cafaea0ff12f6
pllumka <64770203+sl-ruslan@users.noreply.github.com>
2022-06-30 12:15:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Telegraf } = require('telegraf')
 
-const bot = new Telegraf("5409192900:AAFYd6IvCgBA5o4n6VlRlYoVX7XFUY-yFKw")
+const bot = new Telegraf("5452909489:AAGvNGhBZrmkFhghKChXyjUL0bJgiJcK-M8")
 bot.start((ctx) => ctx.reply('Hello, user'))
 bot.hears(`hello`, (ctx) => ctx.reply('Привіт'))
 bot.launch()
